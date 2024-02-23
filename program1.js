e0ff3d695397c49f26bfcc2ce68896cd17382706
Anikesh Ahirwar <118706846+epsalon14@users.noreply.github.com>
2024-02-23 13:37:50 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,9 +3,23 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    const stack = [];
+    const bracketPairs = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
     
-};
-
-module.exports = { isValid };
-
-
+    for (let char of s) {
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        } else {
+            const top = stack.pop();
+            if (bracketPairs[char] !== top) {
+                return false;
+            }
+        }
+    }
+    
+    return stack.length === 0;
+};module.exports = { isValid };
